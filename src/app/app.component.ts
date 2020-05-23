02f0166d5fef2426d592ea675f0c9b249ac1a60a
Jayesh Patil <49228384+jayeshpatil594@users.noreply.github.com>
2020-05-23 19:09:52 +0530

    remove bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model'
-import { from } from 'rxjs';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  storedPosts: Post[] = []
-  title = 'mean-demo'
-  onPostAdded(post){
-    this.storedPosts.push(post)
-  }
+
 }
