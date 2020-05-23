89528c59914ff798c8b750125c5cd73c88cbd288
Jayesh Patil <49228384+jayeshpatil594@users.noreply.github.com>
2020-05-23 19:23:02 +0530

    add PostsService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component'
 
 import { from } from 'rxjs';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostsService } from './posts/posts.service';
 
 @NgModule({
   declarations: [
