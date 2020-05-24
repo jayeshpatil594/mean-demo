4b5b9e622f19e38aad6f295ecf1d881dff3eeb0c
Jayesh Patil <49228384+jayeshpatil594@users.noreply.github.com>
2020-05-24 18:39:26 +0530

    Added Edit and delete features

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post')
 
 const app = express()
 
-mongoose.connect("<your mondodb connection string>")
+mongoose.connect("<your mondodb connection string>",  {useNewUrlParser: true})
   .then(() => {
     console.log('Connected to db')
   }).catch(() => {
@@ -23,20 +23,34 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
     )
   res.setHeader("Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     )
 
   next()
 })
 
-app.post('/api/posts', (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
   })
-  post.save()
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    })
+  })
+})
+
+app.put('/api/posts/:id', (req, res, next) => {
+
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  })
+  Post.updateOne({ _id: req.params.id}, post).then(result => {
+    res.status(200).json({ message: 'Update successful'})
   })
 })
 
@@ -50,10 +64,20 @@ app.get('/api/posts',(req, res, next) => {
     })
 })
 
+app.get('/api/posts/:id', (req, res,next) => {
+  Post.findById(req.params.id).then(post => {
+    if(post){
+      res.status(200).json(post)
+    } else{
+      res.status(404).json({ message: 'Post not found'})
+    }
+  })
+})
+
 app.delete('/api/posts/:id', (req, res, next) => {
   Post.deleteOne({ _id: req.params.id}).then(result => {
     console.log(result)
-    req.statusCode(200).json({ message: 'Post deleted' })
+    res.status(200).json({ message: 'Post deleted' })
   })
 })
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http'
 import { map } from 'rxjs/operators'
 
 import { Post } from './post.model'
+import { PortalHostDirective } from '@angular/cdk/portal'
 
 @Injectable({providedIn: 'root'})
 
@@ -38,6 +39,10 @@ export class PostsService{
     return this.postsUpdated.asObservable()
   }
 
+  getPost(id: string){
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id)
+  }
+
   addPost(title: string, content: string){
     const post: Post = {id: null, title: title, content: content}
     this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
@@ -45,6 +50,18 @@ export class PostsService{
         const id = responseData.postId
         post.id = id
         this.posts.push(post)
+        this.postsUpdated.next([...this.posts])
+      })
+  }
+
+  updatePost(id: string, title: string, content: string){
+    const post: Post = {id: null, title: title, content: content}
+    this.http.put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts]
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id)
+        updatedPosts[oldPostIndex] = post
+        this.posts = updatedPosts
         this.postsUpdated.next([...this.posts])
       })
   }
